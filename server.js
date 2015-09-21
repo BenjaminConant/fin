cc88beb05f8922c45b04969390bb3d6241eea664
Benjamin Conant <conantbenjamin@gmail.com>
2015-09-21 16:12:00 -0400

    updates to placeholder page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser     = require('body-parser');
 var morgan         = require('morgan');
 var methodOverride = require('method-override');
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/client'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
